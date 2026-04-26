7f2e75b4f9a2743e2a9a1ec2f082d9f5326a684b
doggyxomax <doggyxomax@gmail.com>
2026-04-26 22:12:03 +0300

    feat: m2 (for double)

---
+++ include/m2.h
@@ -0,0 +1,16 @@
+#ifndef M2_HEADER
+#define M2_HEADER
+
+#include "m2impl.h"
+#include "v2.h"
+
+m2_header(m2, double, v2);
+
+#endif
+/* =============================== */
+#ifdef M2_SOURCE
+
+m2_source(m2, double);
+
+#undef M2_SOURCE
+#endif
